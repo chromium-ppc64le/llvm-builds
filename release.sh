26307cf43b756f3d85cbbea180eec20d468d8c03
Trung Lê <trung.le@ruby-journal.com>
2021-03-28 04:07:29 +1100

    Add description

--- release.sh
+++ release.sh
@@ -11,4 +11,4 @@ for f in target/*; do
   links+=" --assets-link {\"name\":\"${filename}\",\"url\":\"${PACKAGE_REGISTRY_URL}/${filename}\"}"
 done
 
-./release-cli create --name "Release ${CI_COMMIT_TAG}" --tag-name ${CI_COMMIT_TAG} ${links}
+./release-cli create --name "LLVM ${CI_COMMIT_TAG}" --description "LLVM ${CI_COMMIT_TAG}" --tag-name ${CI_COMMIT_TAG} ${links}
